1a3ca775f5ec8bda28a1f62053aaa49dcec64266
arielrossanigo <arielrossanigo@3722dfcd-5498-485f-68a9-25f413b40af1>
2010-05-12 20:54:42 +0000

    Ping con UDP y mejoras en el codigo

--- Sistemas distribuidos/guia1/src/ClienteUdp.c
+++ Sistemas distribuidos/guia1/src/ClienteUdp.c
@@ -22,50 +22,49 @@ int main(int argc, char* argv[])
 	}
 		
 	// Crear socket
-	int socket_tcp;
+	int socket_udp;
+
 	struct sockaddr_in direccion;
 	
 	direccion.sin_family = AF_INET;
 	direccion.sin_port = htons(puerto);
 	direccion.sin_addr.s_addr = inet_addr(ip_server);
-
-
 	char *msg = generar_paquete(tamanio);
 
 	int l_msg= strlen(msg);
 	char *lee;	
-	lee= malloc(tamanio * sizeof(char));
+	lee= malloc((tamanio + 1) * sizeof(char));
 	int i;
+	int longitud_direccion = sizeof(direccion);	
+	int bytes_leidos;
+	socket_udp = socket (AF_INET, SOCK_DGRAM, 0);
+	if (socket_udp == -1) 
+	{
+		printf("Error, no se pudo crear el socket\n");
+		return -1;
+	}
 
     printf("========================================================\n");
-		
 	for (i =0; i<repeticiones;i++)
 	{
-		socket_tcp = socket (AF_INET, SOCK_STREAM, 0);
-		if (socket_tcp == -1) 
-		{
-			printf("Error, no se pudo crear el socket\n");
-			return -1;
-		}
-
-		if (connect (socket_tcp, (struct sockaddr *) &direccion, sizeof (direccion)) == -1)
-		{
-			printf("Error, no se pudo conectar con el server\n");
-			return -1;
-		}
-
 		iniciar_temporizador();
-		
-		write(socket_tcp, msg, l_msg);		
-		read(socket_tcp, lee, tamanio);
+		sendto (socket_udp, msg, l_msg+1, 0,  (struct sockaddr*) &direccion, longitud_direccion);		
+		bytes_leidos= recvfrom(socket_udp, lee, 10002, 0, (struct sockaddr*) &direccion, &longitud_direccion);
 
+		if (bytes_leidos == -1)
+		{
+			close(socket_udp);
+			printf("Error en la lectura\n");
+			return -1;
+		}		
 		tiempo = finalizar_temporizador();
 		
-		printf("RTT paquete %d: %d uSeg\n", i+1,tiempo);
-		
-		close(socket_tcp);
+		printf("RTT paquete %d: %d uSeg\n", i+1,tiempo);		
 	}
-	imprimir_resultados_ping(ip_server);		
+	close(socket_udp);
+	
+	imprimir_resultados_ping(ip_server);
+
 	return 0;
 }
 
