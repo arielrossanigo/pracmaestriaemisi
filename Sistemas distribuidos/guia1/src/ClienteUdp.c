0bcc72eb739a63058c7d3b59f8c1b4654aa28b52
arielrossanigo <arielrossanigo@3722dfcd-5498-485f-68a9-25f413b40af1>
2010-08-09 19:27:00 +0000

    Entrega final

--- Sistemas distribuidos/guia1/src/ClienteUdp.c
+++ Sistemas distribuidos/guia1/src/ClienteUdp.c
@@ -6,65 +6,65 @@
 #include "utiles_ping.c"
 
 int main(int argc, char* argv[])
-{			
-	char *ip_server;
-	int puerto = 2222;
-	int repeticiones = 5;
-	int tamanio=100;
-    struct ResultadosTemporizador r;
-    iniciar_resultados_temporizador();
-	int tiempo =0;
+{      
+  char *ip_server;
+  int puerto = 2222;
+  int repeticiones = 5;
+  int tamanio=100;
+  iniciar_resultados_temporizador();
 
-	
-	if (parsear_parametros_cliente(argc, argv, &ip_server, &puerto, &repeticiones, &tamanio) < 0)
-	{
-		return -1;
-	}
-		
-	// Crear socket
-	int socket_udp;
+  if (parsear_parametros_cliente(argc, argv, &ip_server, &puerto, &repeticiones, &tamanio) < 0)
+  {
+    return -1;
+  }
+    
+  int socket_udp;
+  struct sockaddr_in direccion;
+  //generamos IP del server
+  direccion.sin_family = AF_INET;
+  direccion.sin_port = htons(puerto);
+  direccion.sin_addr.s_addr = inet_addr(ip_server);
+  
+  //generamos el paquete a enviar
+  char *msg = generar_paquete(tamanio);
+  int l_msg= strlen(msg);
 
-	struct sockaddr_in direccion;
-	
-	direccion.sin_family = AF_INET;
-	direccion.sin_port = htons(puerto);
-	direccion.sin_addr.s_addr = inet_addr(ip_server);
-	char *msg = generar_paquete(tamanio);
+  //creamos buffer de lectura
+  char *lee;  
+  lee= malloc((tamanio + 1) * sizeof(char));
+  
+  int i;
+  int longitud_direccion = sizeof(direccion);  
+  int bytes_leidos;
+  //abrimos un socket
+  socket_udp = socket (AF_INET, SOCK_DGRAM, 0);
+  if (socket_udp == -1) 
+  {
+    printf("Error, no se pudo crear el socket\n");
+    return -1;
+  }
 
-	int l_msg= strlen(msg);
-	char *lee;	
-	lee= malloc((tamanio + 1) * sizeof(char));
-	int i;
-	int longitud_direccion = sizeof(direccion);	
-	int bytes_leidos;
-	socket_udp = socket (AF_INET, SOCK_DGRAM, 0);
-	if (socket_udp == -1) 
-	{
-		printf("Error, no se pudo crear el socket\n");
-		return -1;
-	}
+  printf("========================================================\n");
+  for (i =0; i<repeticiones;i++)
+  {
+    //iniciamos medicion
+    iniciar_temporizador();
+    //escribimos el msg y esperamos la respuesta
+    sendto (socket_udp, msg, l_msg+1, 0,  (struct sockaddr*) &direccion, longitud_direccion);    
+    bytes_leidos= recvfrom(socket_udp, lee, 10002, 0, (struct sockaddr*) &direccion, &longitud_direccion);
 
-    printf("========================================================\n");
-	for (i =0; i<repeticiones;i++)
-	{
-		iniciar_temporizador();
-		sendto (socket_udp, msg, l_msg+1, 0,  (struct sockaddr*) &direccion, longitud_direccion);		
-		bytes_leidos= recvfrom(socket_udp, lee, 10002, 0, (struct sockaddr*) &direccion, &longitud_direccion);
-
-		if (bytes_leidos == -1)
-		{
-			close(socket_udp);
-			printf("Error en la lectura\n");
-			return -1;
-		}		
-		tiempo = finalizar_temporizador();
-		
-		printf("RTT paquete %d: %d uSeg\n", i+1,tiempo);		
-	}
-	close(socket_udp);
-	
-	imprimir_resultados_ping(ip_server);
-
-	return 0;
+    if (bytes_leidos == -1)
+    {
+      close(socket_udp);
+      printf("Error en la lectura\n");
+      return -1;
+    }    
+    //finalizamos la medicion e imprimimos el RTT
+    printf("RTT paquete %d: %d uSeg\n", i+1,finalizar_temporizador());    
+  }
+  close(socket_udp);
+  //imprimimos resultados acumulados
+  imprimir_resultados_ping(ip_server);
+  return 0;
 }
 
--- Sistemas distribuidos/guia3/src/utiles_ping.c
+++ Sistemas distribuidos/guia3/src/utiles_ping.c
@@ -5,124 +5,121 @@
 #include <time.h>
 #include <unistd.h>
 
+//Devuelve el nombre del host
 char * nombre_host()
 {
-	size_t len = 126;
-	char * hostname = malloc(128);
-	gethostname (hostname, len);
-	return hostname;
+  size_t len = 126;
+  char * hostname = malloc(128);
+  gethostname (hostname, len);
+  return hostname;
 }
 
+//completa repeticiones y tamanio de acuerdo a los parametros de entrada
+int parsear_parametros_cliente(int argc, char* argv[], int *repeticiones, int *tamanio)
+{
+  int c;
+  while ((c= getopt(argc, argv, "r::s::")) != -1)
+  {
+    switch(c)
+    {
+      case 'r':
+      {
+        *repeticiones = atoi(optarg);
+        if (*repeticiones < 1 || *repeticiones > 101)
+        {
+          printf ("El nro de repeticiones debe estar comprendido entre 1 y 101\n");
+          return -1;
+        }
+        break;
+      }      
+      case 's':
+      {
+        *tamanio = atoi(optarg);
+        if (*tamanio <1 || *tamanio > 10001)
+        {
+          printf ("El tamano debe estar comprendido entre 1 y 10001\n");
+          return -1;
+        }
+        break;
+      }      
+    }
+  }
+  return 0;  
+}
+
+//Genera un string con los digitos del 0 al 9, de tamanio caracteres
+char* generar_paquete(int tamanio)
+{
+  char* res = malloc((tamanio+1)*sizeof(char));
+  int i;
+  for (i = 0; i<tamanio; i++)
+  {
+    res[i] = (char)(i%10 + 48);
+  }
+  res[tamanio]='\0';
+  return res; 
+}
+
+//Variables utilizadas en los temporizadores
+struct timeval start;      
+struct timeval stop;       
+struct timezone tz;       
+
+//Calcula la diferencia entre dos valores de tiempo y la almacena en out
 void tvsub( out, in )
 struct timeval *out, *in;
 {
-	if( (out->tv_usec -= in->tv_usec) < 0 )   {
-		out->tv_sec--;
-		out->tv_usec += 1000000;
-	}
-	out->tv_sec -= in->tv_sec;
+  if( (out->tv_usec -= in->tv_usec) < 0 )   
+  {
+    out->tv_sec--;
+    out->tv_usec += 1000000;
+  }
+  out->tv_sec -= in->tv_sec;
 }
 
-
-int parsear_parametros_cliente(int argc, char* argv[], int *repeticiones, int *tamanio)
+struct ResultadosTemporizador 
 {
-	int c;
-	while ((c= getopt(argc, argv, "r::s::")) != -1)
-	{
-		switch(c)
-		{
-			case 'r':
-			{
-				*repeticiones = atoi(optarg);
-				if (*repeticiones < 1 || *repeticiones > 101)
-				{
-					printf ("El nro de repeticiones debe estar comprendido entre 1 y 101\n");
-					return -1;
-				}
-				break;
-			}			
-			case 's':
-			{
-				*tamanio = atoi(optarg);
-				if (*tamanio <1 || *tamanio > 10001)
-				{
-					printf ("El tamano debe estar comprendido entre 1 y 10001\n");
-					return -1;
-				}
-				break;
-			}			
-		}
-	}
-	return 0;	
-}
-
-char* generar_paquete(int tamanio)
-{
-	char* res = malloc((tamanio+1)*sizeof(char));
-	int i;
-	for (i = 0; i<tamanio; i++)
-	{
-		res[i] = (char)(i%10 + 48);
-	}
-	res[tamanio]='\0';
-	return res; 
-}
-
-struct timeval start;      /* for timing */
-struct timeval stop;       /* for timing */
-struct timezone tz;       /* for timing */
-struct ResultadosTemporizador {
-    int Minimo;
-    int Maximo;
-    int Total;
-    int Cantidad;
-    float Promedio;
+  int Minimo;
+  int Maximo;
+  int Total;
+  int Cantidad;
+  float Promedio;
 };
 
+//variable para llevar los resultados acumulados del todas las mediciones
 struct ResultadosTemporizador res;
 
+//inicializamos res
 void iniciar_resultados_temporizador()
 {
-    res.Minimo = 0;
-    res.Maximo = -1;
-    res.Cantidad= 0;
-    res.Total = 0;
+  res.Minimo = 0;
+  res.Maximo = -1;
+  res.Cantidad= 0;
+  res.Total = 0;
 }
 
+//Iniciamos una medicion, colocamos en start la hora del dia
 void iniciar_temporizador()
 {
-		gettimeofday( &start, &tz );    
+  gettimeofday( &start, &tz );    
 }
 
+//finalizamos una medicion, calculamos la diferencia entre la hora del dia y start
+//actualizamos los valores acumulados
 int finalizar_temporizador()
 {
-        int tiempo;
-		gettimeofday( &stop, &tz );		
-		tvsub( &stop, &start );		
-		tiempo =  stop.tv_sec * 1000000 + stop.tv_usec;
-		if (res.Minimo > tiempo || res.Cantidad==0)
-		    res.Minimo = tiempo;
-		if (res.Maximo < tiempo || res.Cantidad==0)
-		    res.Maximo = tiempo;
-		res.Total += tiempo;
-		res.Cantidad++;
-		return tiempo;
+  int tiempo;
+  gettimeofday( &stop, &tz );    
+  tvsub( &stop, &start );    
+  tiempo =  stop.tv_sec * 1000000 + stop.tv_usec;
+  if (res.Minimo > tiempo || res.Cantidad==0) res.Minimo = tiempo;
+  if (res.Maximo < tiempo || res.Cantidad==0) res.Maximo = tiempo;
+  res.Total += tiempo;
+  res.Cantidad++;
+  return tiempo;
 }
 
-char * str_hora_actual()
-{
-	struct tm *ptr;
-    time_t lt;
-    //obtenemos tiempo actual
-    lt = time(NULL);
-    //obtenemos tiempo local
-    ptr = localtime(&lt);
-    //obtenemos un string formateado con el tiempo local
-    char * res= malloc(9*sizeof(char));
-    strftime(res, 9, "%H:%M:%S", ptr);
-    return res;
-}
-
+//Obtenemos los resultados acumulados de todas las mediciones
 struct ResultadosTemporizador obtener_resultados_temporizador()
 {
     if (res.Cantidad!= 0)
@@ -132,13 +129,29 @@ struct ResultadosTemporizador obtener_resultados_temporizador()
     return res;
 }
 
+//Imprimimos los resultados acumulados
 void imprimir_resultados()
 {
-    struct ResultadosTemporizador r= obtener_resultados_temporizador();
-    printf("========================================================\n");
-    printf("RTT min: %d uSeg, max: %d uSeg, prom: %.2f uSeg\n", r.Minimo, r.Maximo, r.Promedio);
-    printf("========================================================\n");
+  struct ResultadosTemporizador r= obtener_resultados_temporizador();
+  printf("========================================================\n");
+  printf("RTT min: %d uSeg, max: %d uSeg, prom: %.2f uSeg\n", r.Minimo, r.Maximo, r.Promedio);
+  printf("========================================================\n");
 }
+
+char * str_hora_actual()
+{
+  struct tm *ptr;
+  time_t lt;
+  //obtenemos tiempo actual
+  lt = time(NULL);
+  //obtenemos tiempo local
+  ptr = localtime(&lt);
+  //obtenemos un string formateado con el tiempo local
+  char * res= malloc(9*sizeof(char));
+  strftime(res, 9, "%H:%M:%S", ptr);
+  return res;
+}
+
 
 
 
